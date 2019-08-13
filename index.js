58a10cb609dfd2bc7da1093405e9d25a8ddc62e2
K_Kolev <kkolev@protonmail.com>
2019-08-13 10:02:38 +0300

    chrome and edge fixes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import Typed from "typed.js"
 import humanizeDuration from "humanize-duration"
 
-// there is no error handling here
-document.fonts.ready
-  .then(e => document.body.classList.add("fonts-loaded"))
-  .catch(() => document.body.classList.add("fonts-loaded"))
+// there is no proper error handling here
+document.fonts
+  ? document.fonts.ready
+      .then(e => document.body.classList.add("fonts-loaded"))
+      .catch(() => document.body.classList.add("fonts-loaded"))
+  : document.body.classList.add("fonts-loaded")
 
 let timePassed = 0
 
